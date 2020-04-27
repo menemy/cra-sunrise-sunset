6a4c0dfed4cc07359f8389f1fbe09e0877558c0d
mnagaev <mnagaev@roox.ru>
2020-04-27 23:04:24 +0300

    Updates

---
+++ precache-manifest.539fac3d0036f6ef7702923b181ae21f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeadc0922d575fd1b147e58f00c73c88",
+    "url": "/cra-sunrise-sunset/index.html"
+  },
+  {
+    "revision": "76bc5b8717c3acdde1fc",
+    "url": "/cra-sunrise-sunset/static/css/main.2456ad2d.chunk.css"
+  },
+  {
+    "revision": "a82888b31645562158d2",
+    "url": "/cra-sunrise-sunset/static/js/2.98dd1fa6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cra-sunrise-sunset/static/js/2.98dd1fa6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76bc5b8717c3acdde1fc",
+    "url": "/cra-sunrise-sunset/static/js/main.53396613.chunk.js"
+  },
+  {
+    "revision": "0e315264355eb0fbfbe6",
+    "url": "/cra-sunrise-sunset/static/js/runtime-main.2afc54f2.js"
+  }
+]);
